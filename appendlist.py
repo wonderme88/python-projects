b40cf8a6f44d5a7c0cefa875313251c3173d459c
birender kumar <birender.cdac@gmail.com>
2019-06-19 01:24:28 -0400

    2 files added

---
+++ appendlist.py
@@ -0,0 +1,20 @@
+
+
+list1 = [10,20,30,40]
+
+print "This is simple list"
+print list1
+
+
+
+def addinlist(list):
+	print list
+	list2 = [1,2,3,4,5]
+	list.append(1,2,3)
+	print list
+	return
+
+
+print(" Add few more contents in list")
+list1 = addinlist(list1)
+print("After adding few more thing in list")
---
+++ revstring.py
@@ -0,0 +1,26 @@
+
+
+
+def revStr(value):
+	return value[::-1]
+
+
+str1 = "my name is chanchal"
+str2 = "birender"
+str3 = "chanchal"
+
+#print revStr(str1)
+
+
+list = []
+
+list.append(str1)
+list.append(str2)
+list.append(str3)
+
+print (list)
+
+for x in list:
+	print revStr(x)
+
+
