7757854e46d213f50b73743c4286b17f56e2c9b2
wonderme88 <wonderme.88@gmail.com>
2019-11-19 12:02:49 -0500

    add module example

---
+++ module/demoCalc.py
@@ -0,0 +1,21 @@
+#import calc
+#from calc import add
+from calc import *
+
+a=9
+b=7
+
+sum1 = add(a,b)
+print (sum1)
+
+sub1 = sub(a,b)
+print (sub1)
+
+mul = multi(a,b)
+print (mul)
+
+divi = div(a,b)
+print (divi)
+
+
+
