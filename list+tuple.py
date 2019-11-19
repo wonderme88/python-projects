85c036abc9df6eaf5daaf56333f1d9c9eda0d6e0
wonderme88 <wonderme.88@gmail.com>
2019-11-19 10:30:14 -0500

    added tuple+list cases

---
+++ list+tuple.py
@@ -0,0 +1,15 @@
+
+list1 = [10, 20 , 30, (1,2,3,4)]
+print list1
+
+#chnage the list 
+list2 = ["red", "green", "blue"]
+
+list1.append(list2)
+
+print list1
+
+#list1[3].append(5)   ==> tuple object has no attribute append
+
+
+
---
+++ tuple+list.py
@@ -0,0 +1,9 @@
+
+tup1 = (10, 20, 30, [1,2,3,4])
+print tup1
+
+#Modifyj the list inside the tuple.
+tup1[3].append(5)
+print tup1
+
+#tup1[0]=100 ===> error
